ff8b51ee511972a207895ee28dd2bc68281fb852
RaynorHeringer <30930010+RaynorHeringer@users.noreply.github.com>
2025-05-14 19:39:41 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,16 +18,16 @@ const Hero = () => {
       <div className="container mx-auto px-4 z-10 pt-20">
         <div className="max-w-3xl">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6">
-            Projetos e Aprovações para sua Obra na Região dos Lagos
+            O seu projetos para sua Obra.
           </h1>
           <p className="text-xl text-white opacity-90 mb-8">
-            Especialistas em desenvolvimento de projetos e processos de aprovação junto às prefeituras da Região dos Lagos, Saquarema.
+            Especialistas em desenvolvimento de projetos para aprovação e econstrução na Região dos Lagos,
           </p>
           <div className="flex flex-col sm:flex-row gap-4">
-            <Button size="lg" className="bg-white text-heringer-blue hover:bg-gray-100">
+            <Button size="lg" variant="outline" className="bg-white text-heringer-blue hover:bg-gray-100">
               Nossos Serviços
             </Button>
-            <Button size="lg" variant="outline" className="text-white border-white hover:bg-white/10">
+            <Button size="lg" variant="outline" className="text-blue border-white hover:bg-white/10">
               Fale Conosco <ArrowRight className="ml-2 h-4 w-4" />
             </Button>
           </div>
